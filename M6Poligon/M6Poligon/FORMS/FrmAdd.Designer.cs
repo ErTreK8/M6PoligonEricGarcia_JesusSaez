4f4eb9558b3a54ae586a64e61773dd3f720fbfe8
Jesusaez <jesusmiguel.saezv@educem.net>
2025-02-11 07:58:51 +0100

    CREACION FORM Y INTEGRACION BASE DE DATOS

---
+++ M6Poligon/M6Poligon/FORMS/FrmAdd.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace M6Poligon.FORMS
+{
+    partial class FrmAdd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbPoligons = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.cdColorPol = new System.Windows.Forms.ColorDialog();
+            this.chkPle = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbPoligons
+            // 
+            this.cbPoligons.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbPoligons.FormattingEnabled = true;
+            this.cbPoligons.Location = new System.Drawing.Point(105, 36);
+            this.cbPoligons.Name = "cbPoligons";
+            this.cbPoligons.Size = new System.Drawing.Size(260, 24);
+            this.cbPoligons.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkPle);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Location = new System.Drawing.Point(23, 123);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(449, 346);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(61, 83);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(247, 83);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown2.TabIndex = 1;
+            // 
+            // chkPle
+            // 
+            this.chkPle.AutoSize = true;
+            this.chkPle.Location = new System.Drawing.Point(246, 155);
+            this.chkPle.Name = "chkPle";
+            this.chkPle.Size = new System.Drawing.Size(49, 20);
+            this.chkPle.TabIndex = 2;
+            this.chkPle.Text = "Ple";
+            this.chkPle.UseVisualStyleBackColor = true;
+            this.chkPle.CheckedChanged += new System.EventHandler(this.chkPle_CheckedChanged);
+            // 
+            // FrmAdd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 498);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cbPoligons);
+            this.Name = "FrmAdd";
+            this.Text = "FrmAdd";
+            this.Load += new System.EventHandler(this.FrmAdd_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbPoligons;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ColorDialog cdColorPol;
+        private System.Windows.Forms.CheckBox chkPle;
+    }
+}
---
+++ M6Poligon/M6Poligon/FORMS/FrmAdd.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace M6Poligon.FORMS
+{
+    public partial class FrmAdd : Form
+    {
+        bdPoligonsEntities poligonsContext;
+
+        public FrmAdd()
+        {
+            InitializeComponent();
+        }
+
+        private void chkPle_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkPle.Checked) chkPle.Text = "Ple";
+            else chkPle.Text = "Buit";
+        }
+
+        private void FrmAdd_Load(object sender, EventArgs e)
+        {
+            getDades();
+        }
+
+        private void getDades()
+        {
+            var poligons = (from p in poligonsContext.);
+
+        }
+    }
+}
